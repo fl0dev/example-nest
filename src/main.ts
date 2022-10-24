46575ce9dfce78e4887f66c1892f83a6dd466d7c
jharrisonio <james@jharrison.io>
2022-10-24 14:44:19 -0700

    Removed hardcoded port and defaulted to 80

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 80;
+  console.log(`NestJS app listening on port ${port}`);
+  await app.listen(port);
 }
 bootstrap();

996dbfc6d1fbca017859085c3d6fd5b5c070113d
jharrisonio <james@jharrison.io>
2022-10-24 14:59:18 -0700

    Added healthcheck endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+type HealthcheckResponse = {
+  status: string;
+  errors: string[];
+};
+
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('healthcheck')
+  getHealthcheck(): HealthcheckResponse {
+    return { status: 'OK', errors: [] };
   }
 }

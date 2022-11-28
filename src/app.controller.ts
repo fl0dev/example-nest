32d4a8a6035795b2777f9786d9e6683ed699632d
jharrisonio <james@jharrison.io>
2022-11-28 11:56:29 -0800

    Added endpoint for testing file uploads

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 type HealthcheckResponse = {
   status: string;
@@ -13,6 +14,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.appService.handleFileUpload(file);
   }
 
   @Get('healthcheck')
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,9 @@ export class AppService {
   getHello(): string {
     return 'v0.2.0';
   }
+
+  handleFileUpload(file: Express.Multer.File) {
+    console.log(file);
+    return file.originalname;
+  }
 }
